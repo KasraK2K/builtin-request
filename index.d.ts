25242c3564d656a346a72972724f21126256c009
Kasra Karami <Kasra.Karami.Work@gmail.com>
2023-06-24 14:30:24 +0330

    fix exporter

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,22 @@
 declare class Request {
   constructor(baseUrl?: string)
 
-  execute(options: IOpions): Promise<any>
+  execute(options: IOptions): Promise<any>
 
-  get(path: string, options?: IOpions): Promise<any>
+  get(path: string, options?: IOptions): Promise<any>
 
-  post(path: string, data?: any, options?: IOpions): Promise<any>
-  put(path: string, data?: any, options?: IOpions): Promise<any>
-  patch(path: string, data?: any, options?: IOpions): Promise<any>
-  delete(path: string, data?: any, options?: IOpions): Promise<any>
+  post(path: string, data?: any, options?: IOptions): Promise<any>
+  put(path: string, data?: any, options?: IOptions): Promise<any>
+  patch(path: string, data?: any, options?: IOptions): Promise<any>
+  delete(path: string, data?: any, options?: IOptions): Promise<any>
 }
 
-interface IOpions {
+interface IOptions {
   hostname?: string
   port?: number
   path?: string
   method?: string
 }
 
-export { IOpions }
+export { IOptions as IOpions }
 export default Request
